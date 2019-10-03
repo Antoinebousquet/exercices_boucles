078e3785b5317f8f5e6562292e39832f31abc740
Gaël DUBOIS <gael.dubois.pro@gmail.com>
2019-10-03 12:19:37 +0200

    pyramid input passé en méthode avec le return / gael

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -1,8 +1,9 @@
-# commentaire test
-puts "Quelle hauteur la pyramide ?"
-puts "(nombre entier compris entre 1 et 25)"
-begin
-    print ">>"
-    height = Integer(gets) rescue 0
-end while height > 25 || height < 1
-
+    def pyramid_input
+    puts "Quelle hauteur doit faire la pyramide ?"
+    puts "(nombre entier compris entre 1 et 25)"
+    begin
+        print ">>"
+        height = Integer(gets) rescue 0
+    end while height > 25 || height < 1
+    return height
+end
