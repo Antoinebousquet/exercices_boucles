17f2144bc24fc9e1b6e2c908a329e2cc84cfab19
Gaël DUBOIS <gael.dubois.pro@gmail.com>
2019-10-03 12:17:31 +0200

    pyramid input rajouté / gael

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -1,1 +1,8 @@
 # commentaire test
+puts "Quelle hauteur la pyramide ?"
+puts "(nombre entier compris entre 1 et 25)"
+begin
+    print ">>"
+    height = Integer(gets) rescue 0
+end while height > 25 || height < 1
+
